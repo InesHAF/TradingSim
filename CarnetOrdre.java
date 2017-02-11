4e02ac4d39661cc8840984f2f5cb3d839f8a2c25
Steve DIAS <stevediaspro@gmail.com>
2017-02-11 13:03:30 +0100

    Mise à jour Carnet Ordre et Ordre

--- CarnetOrdre.java
+++ CarnetOrdre.java
@@ -1,39 +1,151 @@
 package tradingsim;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+
+
+
 public class CarnetOrdre {
     
-    private Entreprise entreprise;
+
+	private Queue<OrdrePrix> achatqueue = new PriorityQueue<OrdrePrix>(100, new Comparator<OrdrePrix>() {
+		public int compare(OrdrePrix int1, OrdrePrix int2) {
+			Double prix1 = int1.getPrix();
+			Double prix2 = int2.getPrix();
+			return prix2.compareTo(prix1);
+		}
+	});
+	private Queue<OrdrePrix> ventequeue = new PriorityQueue<OrdrePrix>(100, new Comparator<OrdrePrix>() {
+		public int compare(OrdrePrix int1, OrdrePrix int2) {
+			Double prix1 = int1.getPrix();
+			Double prix2 = int2.getPrix();
+			return prix1.compareTo(prix2);
+		}
+	});
+
+	public CarnetOrdre() {
+
+	}
+
+	public Queue<OrdrePrix> getAchatqueue() {
+		return achatqueue;
+	}
+
+	public void setAchatqueue(Queue<OrdrePrix> achatqueue) {
+		this.achatqueue = achatqueue;
+	}
+
+	public Queue<OrdrePrix> getVentequeue() {
+		return ventequeue;
+	}
+
+	public void setVentequeue(Queue<OrdrePrix> ventequeue) {
+		this.ventequeue = ventequeue;
+	}
 
 
-    private Ordre listeOrdreA;
 
+	public int addOrdre(Ordre ordre) {
+		if (ordre.isAchat()) {
+			OrdrePrix ordreprix = containAchat(ordre.getPrixunitaire());
+			if (ordreprix != null) {
+				return ordreprix.addOrdre(ordre);
+			} else {
+				ordreprix = new OrdrePrix(ordre);
+				achatqueue.add(ordreprix);
+				return 1;
+			}
+		} else {
+			OrdrePrix ordreprix = containVente(ordre.getPrixunitaire());
+			if (ordreprix != null) {
+				return ordreprix.addOrdre(ordre);
+			} else {
+				ordreprix = new OrdrePrix(ordre);
+				ventequeue.add(ordreprix);
+				return 1;
+			}
 
-    private String listeOrdreV;
+		}
 
+	}
 
-    public OrdrePrix ordrePrix;
+	public OrdrePrix containAchat(double prix) {
 
+		Iterator<OrdrePrix> iterator = achatqueue.iterator();
+		while (iterator.hasNext()) {
+			OrdrePrix ordreprix = iterator.next();
+			if (ordreprix.getPrix() == prix) {
+				return ordreprix;
+			}
+		}
+		return null;
+	}
 
-    public void addOrdre(Ordre ordre) {
-    }
+	public OrdrePrix containVente(double prix) {
+		Iterator<OrdrePrix> iterator = ventequeue.iterator();
+		while (iterator.hasNext()) {
+			OrdrePrix ordreprix = iterator.next();
+			if (ordreprix.getPrix() == prix) {
+				return ordreprix;
+			}
+		}
+		return null;
+	}
+	
+	public int removeOrdre(Ordre ordre){
+		if (ordre.isAchat()){
+			Iterator<OrdrePrix> iterator = achatqueue.iterator();
+			while(iterator.hasNext()){
+				OrdrePrix ordreprix= iterator.next();
+				
+				if(ordreprix.getListeordre().containsValue(ordre)){
+					ordreprix.getListeordre().remove(ordre.getKey());
+					ordreprix.actualiserQuantite();
+					if(ordreprix.getQuantitetotale()==0){
+						achatqueue.remove(ordreprix);
+					}
+					return 1;
+				}
+			}
+				return 0;
+			
+		}else{
+			Iterator<OrdrePrix> iterator = ventequeue.iterator();
+			while(iterator.hasNext()){
+				OrdrePrix ordreprix= iterator.next();
+				
+				if(ordreprix.getListeordre().containsValue(ordre)){
+					ordreprix.getListeordre().remove(ordre.getKey());
+					ordreprix.actualiserQuantite();
+					if(ordreprix.getQuantitetotale()==0){
+						achatqueue.remove(ordreprix);
+					}
+					return 1;
+				}
+				
+			}
+			return 0;
+		}
+	}
 
-
-    public void addOrdrePrix(OrdrePrix ordrePrix) {
-    }
-
-
-    public boolean trouverOrdrePrix(double prix, boolean type) {
-    }
-
-
-    public boolean removeOrdrePrix(double prix, boolean type) {
-    }
-
-
-    public void actualiserPrixAction() {
-    }
-
-
-    public boolean match() {
-    }
+	public void affichage() {
+		Iterator<OrdrePrix> iterator = achatqueue.iterator();
+		System.out.println("-----CarnetOrdre-----\n");
+		System.out.println("-----Achats-----\n");
+		while (iterator.hasNext()) {
+			OrdrePrix ordreprix = iterator.next();
+			System.out.println(ordreprix);
+		}
+		iterator = ventequeue.iterator();
+		System.out.println("-----Ventes-----\n");
+		while (iterator.hasNext()) {
+			OrdrePrix ordreprix = iterator.next();
+			System.out.println(ordreprix);
+		}
+	}
+	
 
 }
--- Ordre.java
+++ Ordre.java
@@ -1,18 +1,64 @@
 package tradingsim;
 
 public class Ordre {
-    private Entreprise entreprise;
 
-    private int qte;
+	private boolean contrepartie;
 
-    private String prixUnitaire;
+	private String entreprise;
+	private double prixunitaire;
+	private int quantite;
+	private boolean achat;
+	private int key = -1;
 
-    private boolean oUtilisateur;
+	public Ordre(String entreprise, double prixunitaire, boolean achat, int quantite) {
+		this.entreprise = entreprise;
+		this.prixunitaire = prixunitaire;
+		this.achat = achat;
+		this.quantite = quantite;
+	}
 
-    private boolean oAchat;
+	public int getKey() {
+		return key;
+	}
 
-    private int idU;
+	public void setKey(int key) {
+		this.key = key;
+	}
 
-    private boolean contrepartie;
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	public String getEntreprise() {
+		return entreprise;
+	}
+
+	public void setEntreprise(String entreprise) {
+		this.entreprise = entreprise;
+	}
+
+	public double getPrixunitaire() {
+		return prixunitaire;
+	}
+
+	public void setPrixunitaire(double prixunitaire) {
+		this.prixunitaire = prixunitaire;
+	}
+
+	public boolean isAchat() {
+		return achat;
+	}
+
+	public void setAchat(boolean achat) {
+		this.achat = achat;
+	}
+
+	public String toString() {
+		return "[" + entreprise + ";" + prixunitaire + "]";
+	}
 
 }
